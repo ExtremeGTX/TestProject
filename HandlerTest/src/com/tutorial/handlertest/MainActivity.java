4549b4ad7ddb110f7840bad7e962054a2ad43d78
ExtremeGTX <ExtremeGTX@hotmail.com>
2014-03-07 23:49:09 +0200

    Code Formatting

--- HandlerTest/src/com/tutorial/handlertest/MainActivity.java
+++ HandlerTest/src/com/tutorial/handlertest/MainActivity.java
@@ -1,30 +1,29 @@
 package com.tutorial.handlertest;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Handler.Callback;
 import android.os.Message;
-import android.app.Activity;
 import android.util.Log;
 
-public class MainActivity extends Activity implements Callback{
-	
+public class MainActivity extends Activity implements Callback {
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-			
+
 		// Setup UIHandler and static Class MainHandler
-		new MainHandler(new Handler(getMainLooper(),this));
+		new MainHandler(new Handler(getMainLooper(), this));
 		// Launch a thread for testing our handler
 		new Thread(new TestThread()).start();
 	}
 
-	
 	@Override
 	public boolean handleMessage(Message msg) {
 		// TODO Auto-generated method stub
-		Log.v("MyApp","Msg From Thread: " + msg.toString());
+		Log.v("MyApp", "Msg From TestThread: " + msg.toString());
 		return false;
 	}
 
